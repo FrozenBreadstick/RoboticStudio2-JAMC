670f70062679a5a7ce5cf3737bb6e1b3be2f5d57
Frozen Breadstick <mattia.sydcanfam@gmail.com>
2026-03-12 13:48:02 +1100

    Various changes due to git conflicts

---
+++ src/midi_processor/midi_processor.h
@@ -0,0 +1,12 @@
+// includes
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+#include <thread>
+#include <mutex>
+#include <condition_variable>
+
+#include "midifile/lib/libmidifile.a"
+
