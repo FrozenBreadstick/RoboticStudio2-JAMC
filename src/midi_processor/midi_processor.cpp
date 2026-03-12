9d29da9074e7be3f8aff9b88c9396033ecf0f019
connor <starr.drakon@gmail.com>
2026-03-12 13:32:49 +1100

    added midi file reading library and midi processing

---
+++ src/midi_processor/midi_processor.cpp
@@ -0,0 +1,15 @@
+// includes -------------------------------------------------------------------
+#include "midi_processor.h"
+
+// constructor ----------------------------------------------------------------
+MidiProcessor::MidiProcessor(std::string midi_file_path) {
+
+    // create midi
+
+}
+
+
+// functions ------------------------------------------------------------------
+
+// open file
+void MidiProcessor::open_file(std::string midi_file_path) {
