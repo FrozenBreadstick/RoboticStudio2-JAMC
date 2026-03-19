f6ec328c31a0c89e7dbb1c3b6d57c692752233f4
Frozen Breadstick <mattia.sydcanfam@gmail.com>
2026-03-19 15:04:21 +1100

    Added some custom services for controller control

--- include/control/controller.h
+++ include/control/controller.h
@@ -3,7 +3,25 @@
 
 #include "external/json.hpp"
 #include "rclcpp/rclcpp.hpp"
+#include "srv/load.hpp"
+#include "srv/func.hpp"
+#include "srv/time_scale.hpp"
 
+using nlohmann::json;
+
+namespace Control
+{
+    class Controller : public rclcpp::Node
+    {
+    public:
+        Controller();
+        ~Controller();
+
+    private:
+        
+
+    };
+}
 
 
 #endif // CONTROLLER_H
---
+++ src/control/controller_main.cpp
@@ -0,0 +1,8 @@
+#include <rclcpp/rclcpp.hpp>
+
+int main(int argc, char* argv[])
+{
+    auto argc_ = argc; // DO NOT WORRY ABOUT THESE WARNINGS REGARDING UNUSED VARIABLES
+    auto argv_ = argv; // DO NOT WORRY ABOUT THESE WARNINGS REGARDING UNUSED VARIABLES
+    return 0;
+}
