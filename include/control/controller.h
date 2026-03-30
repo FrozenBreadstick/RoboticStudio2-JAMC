4573216a662cc1281b88cc09517023680516a6a0
Frozen Breadstick <mattia.sydcanfam@gmail.com>
2026-03-30 23:54:28 +1100

    Added prototype of controller class, added doxygen scaffold, added doxygen tags and comments to controller class

--- include/control/controller.h
+++ include/control/controller.h
@@ -3,22 +3,61 @@
 
 #include "external/json.hpp"
 #include "rclcpp/rclcpp.hpp"
-#include "srv/load.hpp"
-#include "srv/func.hpp"
-#include "srv/time_scale.hpp"
+#include "jamc/srv/load.hpp"
+#include "jamc/srv/func.hpp"
+#include "jamc/srv/time_scale.hpp"
+#include <geometry_msgs/msg/twist_stamped.hpp>
+#include <geometry_msgs/msg/point.hpp>
+#include <chrono>
+#include <cmath>
 
 using nlohmann::json;
 
 namespace Control
 {
+    struct vector3
+    {
+        double x;
+        double y;
+        double z;
+    };
+
     class Controller : public rclcpp::Node
     {
     public:
         Controller();
         ~Controller();
 
+        void sendVector(const vector3 &vec);
+        void sendStop();
+
+        int activeTrackDebug(vector3 target, bool x = false, bool y = false, bool z = false);
+
     private:
-        
+        //Methods
+        void sendTwistMsg(double x, double y, double z, double angular_x = 0.0, double angular_y = 0.0, double angular_z = 0.0);
+
+        //Publishers
+        rclcpp::Publisher<geometry_msgs::msg::TwistStamped>::SharedPtr twist_pub_;
+
+        //Subscribers
+        rclcpp::Subscription<geometry_msgs::msg::Point>::SharedPtr debug_target_sub_;
+        void debug_target_callback(const geometry_msgs::msg::Point::SharedPtr msg);
+
+        //Services
+        /**
+         * @name ROS2 Services
+         * Services that the Controller Class provides
+         */
+        ///@{
+        rclcpp::Service<jamc::srv::Load>::SharedPtr load_service_;
+        rclcpp::Service<jamc::srv::TimeScale>::SharedPtr time_service_;
+        rclcpp::Service<jamc::srv::Func>::SharedPtr play_pause_service_;
+        ///@}
+
+
+        //Timers
+        rclcpp::TimerBase::SharedPtr control_timer_;
 
     };
 }
---
+++ src/control/controller.cpp
@@ -0,0 +1,107 @@
+#include "control/controller.h"
+
+
+/**
+ * @brief Standalone Class for Controlling a UR3e. Typically reads data from .mipi files for plaback on a piano. Exposes various services for control.
+ * 
+ * @details This class contains the following ROS2 I/O
+ * - **Services**
+ *  - '-/MIPI_Controller/load' (jamc/srv/Load): Takes a string which should be the filepath to a .mipi file to be loaded, and an integer that is the index of the instrument to play.
+ *  - '-/MIPI_Controller/time_scale' (jamc/srv/TimeScale): Take a float between -1 and 1 used to scale the speed that the robot plays at (Negative numbers cause the robot to play in reverse)
+ *  - '-/MIPI_Controller/play_pause' (jamc/srv/Func): Takes an empty, this is a trigger service used to play and pause the current track
+ */
+Control::Controller::Controller() : Node("MIPI_Controller")
+{
+    RCLCPP_INFO(this->get_logger(), "Controller node has been started.");
+    twist_pub_ = this->create_publisher<geometry_msgs::msg::TwistStamped>("/servo_node/delta_twist_cmds", 10);
+    debug_target_sub_ = this->create_subscription<geometry_msgs::msg::Point>("/debug_target", 10, std::bind(&Controller::debug_target_callback, this, std::placeholders::_1));
+}
+
+/**
+ * @brief Standard Destructor for Controller Class
+ */
+Control::Controller::~Controller()
+{
+    RCLCPP_INFO(this->get_logger(), "Controller node is shutting down.");
+}
+
+/**
+ * @brief A convenience method that neatly packs up a twist messge and publishes it to the UR_Driver
+ * @param x A double for translational velocity of the End Effector in the **X** axis
+ * @param y A double for translational velocity of the End Effector in the **Y** axis
+ * @param z A double for translational velocity of the End Effector in the **Z** axis
+ * @param angular_x A double for rotational velocity of the End Effector in the **X** axis (Optional, Defaults to 0.0)
+ * @param angular_y A double for rotational velocity of the End Effector in the **Y** axis (Optional, Defaults to 0.0)
+ * @param angular_z A double for rotational velocity of the End Effector in the **Z** axis (Optional, Defaults to 0.0)
+ */
+void Control::Controller::sendTwistMsg(double x, double y, double z, double angular_x, double angular_y, double angular_z)
+{
+    auto msg = geometry_msgs::msg::TwistStamped();
+    msg.header.stamp = this->now();
+    msg.header.frame_id = "base_link"; 
+
+    msg.twist.linear.x = x;
+    msg.twist.linear.y = y;
+    msg.twist.linear.z = z;
+
+    msg.twist.angular.x = angular_x;
+    msg.twist.angular.y = angular_y;
+    msg.twist.angular.z = angular_z;
+
+    twist_pub_->publish(msg);
+
+    RCLCPP_INFO(this->get_logger(), "Sending Twist message: linear=(%.2f, %.2f, %.2f), angular=(%.2f, %.2f, %.2f)", x, y, z, angular_x, angular_y, angular_z);
+}
+
+/**
+ * @brief The public method for sending translational End Effector vector commands to the UR3e
+ * @param vec Takes a vector3 struct (included in this class, or declared using {x, y, z})
+ */
+void Control::Controller::sendVector(const vector3 &vec)
+{
+    sendTwistMsg(vec.x, vec.y, vec.z);
+}
+
+/**
+ * @brief A simple helper method for immediately sending stop to the UR3e
+ */
+void Control::Controller::sendStop()
+{
+    sendTwistMsg(0.0, 0.0, 0.0);
+}
+
+/**
+ * @brief A method for enabling "activeTrack" mode, allowing for live visual servoing following a topic
+ * @param target A vector3 struct that is the target
+ * @param x A bool to enable motion in the X plane
+ * @param y A bool to enable motion in the Y plane
+ * @param z A bool to enable motion in the Z plane
+ */
+int Control::Controller::activeTrackDebug(vector3 target, bool x, bool y, bool z)
+{
+    if (!x) target.x = 0.0;
+    if (!y) target.y = 0.0;
+    if (!z) target.z = 0.0;
+    sendVector(target);
+    return 0;
+}
+
+/**
+ * @brief Callback function for catching the debug subscriber, used to activate activeTrack
+ * @param msg Input from the topic subscription
+ */
+void Control::Controller::debug_target_callback(const geometry_msgs::msg::Point::SharedPtr msg)
+{
+    RCLCPP_INFO(this->get_logger(), "Received debug target: (%.2f, %.2f, %.2f)", msg->x, msg->y, msg->z);
+    //Clamp values for speed safety
+    double scaling = 2.0;
+    vector3 vec;
+    double max = std::max({std::abs(msg->x), std::abs(msg->y), std::abs(msg->z)});
+    if (max > 2.0) {
+        vec.x = (msg->x/max) * scaling;
+        vec.y = (msg->y/max) * scaling;
+        vec.z = (msg->z/max) * scaling;
+    }
+    activeTrackDebug(vec, true, true);
+
+}
